bf50504873cc83db59e22c9d73e01efb2cd80ca0
WangtongYa <1156187423@qq.com>
2021-02-05 19:26:03 +0800

    看我新增俩文件

--- src/dao/AddLinkManDao.java
+++ src/dao/AddLinkManDao.java
@@ -8,5 +8,6 @@ import java.util.List;
 public interface AddLinkManDao {
     void addLinkMan(LinkMan linkMan);
 
+    // 试试看 看颜色变了
     List<LinkMan> getAll(DetachedCriteria dc);
 }
---
+++ src/dao/impl/test.java
@@ -0,0 +1,4 @@
+package dao.impl;
+// 新建的是红的 改了的是蓝的 然后我们提交一下
+public class test {
+}
